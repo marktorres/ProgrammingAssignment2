7d56cc075c452add235e6322a6d969b71902849c
marktorres <mgt400@bellsouth.net>
2014-07-27 16:04:57 -0500

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions that will cache the inverse of a matrix. 
+## Matrix inversion is usually a costly computation and there may be some
+## benefit to caching the inverse of a matrix rather than computing it
+## repeatedly.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## The following function, makeCacheMatrix creates a special "matrix" object
+## that can cache its inverse and a vector that contains a list which will: 
+##
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse of the matrix
+## 4.  get the value of the inverse of the matrix
+
+
+makeCacheMatrix <- function(max = matrix()) {
+
+     invmax <- NULL
+
+     ## Define the four functions to maintain the cache
+     setmax <- function(newmax) {
+          ## Save the matrix into the cache
+          max <<- newmax
+          invmax <<- NULL
+     }
+     getmax <- function() {max}
+     ## Save the inverse of the matrix into the cache
+     setinvmax <- function(newinvmax) {invmax <<- newinvmax}
+     getinvmax <- function() {invmax}
+
+     ## Define the list containing the four functions
+     list(setmax = setmax, 
+          getmax = getmax,
+          setinvmax = setinvmax,
+          getinvmax = getinvmax)
 }
 
 
-## Write a short comment describing this function
+## The second function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then the
+## `cachesolve` will retrieve the inverse from the cache and skips 
+## the computation of the inverse matrix.  Otherwise, it calculates 
+## the inverse of the matrix and sets the value of the cache via the 
+## `setinvmax` function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(max, ...) {
+     ## Retrive the matix and inverse matrix from the cache
+     curmax <- max$getmax()
+     invmax <- max$getinvmax()
+
+     ## Check to see if the matrix and matrix in the cache are identical     
+     if(identical(curmax, max)) {
+          message("getting cached inverse matrix")
+          return(invmax)
+     }
+     ## The matrix and matrix in the cache are not identical
+     message("calculating inverse matrix")
+     invmax <- solve (max, ...)
+     max$setinvmax(invmax)
+
+     invmax
 }
